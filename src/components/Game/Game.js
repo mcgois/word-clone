0267ed8b3e1aa0585b1f9f9be4c0f8dc82a35fb8
Matheus Cabral <matheus.cabral@gmail.com>
2023-02-06 09:23:15 -0300

    5

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 
 import { WORDS } from "../../data";
 import { sample } from "../../utils";
@@ -12,6 +13,21 @@ console.info({ answer });
 function Game() {
   const [guess, setGuess] = useState("");
   const [guessList, setGuessList] = useState([]);
+
+  const result = checkResult();
+
+  function checkResult() {
+    if (
+      guessList.length >= NUM_OF_GUESSES_ALLOWED &&
+      guessList.filter((guess) => guess.guess === answer).length === 0
+    ) {
+      return false;
+    }
+    if (guessList.filter((guess) => guess.guess === answer).length > 0) {
+      return true;
+    }
+    return undefined;
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -46,8 +62,24 @@ function Game() {
           }}
           maxLength={5}
           minLength={5}
+          disabled={result === false}
         />
       </form>
+      {result === true && (
+        <div className="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in
+            <strong> {guessList.length} guesses</strong>.
+          </p>
+        </div>
+      )}
+      {result === false && (
+        <div className="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>{answer}</strong>.
+          </p>
+        </div>
+      )}
     </>
   );
 }
